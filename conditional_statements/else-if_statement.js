a21ca65fbb02d5511cbd033969e27b7a6a95f376
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-08 01:17:39 +0530

    Add files via upload

---
+++ conditional_statements/else-if_statement.js
@@ -0,0 +1,18 @@
+let mode=light;
+if(mode=="dark")
+    {
+       color=="balck";
+    }
+    else if(mode=="red")
+        {
+           color=="red";
+        }
+        else if(mode=="green")
+            {
+                color=="green"
+            }
+            else
+            {
+                color=="white"
+            }
+            console.log(color);
