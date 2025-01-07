69fe6350dd534db1beec9153c3626d09ffb3e979
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-08 01:17:12 +0530

    Add files via upload

---
+++ conditional_statements/if-else.statement.js
@@ -0,0 +1,23 @@
+// let mode="light";
+//         let color;
+//         if(mode=="dark")
+//             {
+//                 color="black";
+//             }
+//             else
+//                 {
+//                     color="white";
+//                 }
+//                 console.log(color);
+
+
+//vote eligibility
+// let age=16;
+// if(age>=18)
+//   {
+//     console.log("Can vote");
+//     }
+//  else
+//      {
+//      console.log("Can't vote");
+//      }
