6b8f6e0cf528f47581e6aa36a71c5ee56e71711b
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-10 11:46:02 +0530

    Add files via upload

---
+++ practice_questions/q2.js
@@ -0,0 +1,10 @@
+const profile={
+    userName: "shradhakhapra",
+    num_posts: 195,
+    num_followers: 569000,
+    num_following: 4,
+    name: " Shradha Khapra",
+    account_type: "Enterprenuer",
+    bio: "Apna College| Ex-microsoft, DRDO| To educate someone is the highest priviledge!"
+};
+console.log(typeof profile["userName"]);
