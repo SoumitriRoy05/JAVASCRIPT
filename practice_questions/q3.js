866a14b93d181ebcf7e9fbad0a19e3e609b4c606
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-11 02:10:31 +0530

    Add files via upload

---
+++ practice_questions/q3.js
@@ -0,0 +1,9 @@
+let num= prompt("Enter a number:");
+if(num%5==0)
+    {
+        console.log("num is multiple of 5");
+    }
+    else
+    {
+        console.log("num isn't multiple of 5");
+    }
