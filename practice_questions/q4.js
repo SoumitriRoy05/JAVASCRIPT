2bd67b30d71c975ec6bde06a03a4be62eefe35a2
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-11 02:14:39 +0530

    Update q4.js

--- practice_questions/q4.js
+++ practice_questions/q4.js
@@ -1,3 +1,12 @@
+// Write a program that takes a numeric score (marks) as input and assigns a grade based on predefined ranges. The program should then display the grade corresponding to the entered score.
+// Grading Criteria:
+// A: Marks between 80 and 100 (inclusive)
+// B: Marks between 70 and 89 (inclusive)
+// C: Marks between 60 and 69 (inclusive)
+// D: Marks between 50 and 59 (inclusive)
+// F: Marks below 50
+
+
 let grade=70;
 if(grade>=80 && grade<=100)
     {
