4eab4569bf2ecec5417e0405d4602131c8b3d71b
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-12 20:09:18 +0530

    Add files via upload

---
+++ practice_questions/q6.js
@@ -0,0 +1,7 @@
+let gameNum=24;
+let userName=prompt("Guess the game number:");
+while(userName!=gameNum)
+    {
+userName= prompt("u entered wrong num, enter again");
+    }
+    console.log("u entered the rgt number");
