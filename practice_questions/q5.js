a972ab800513971ad7ceecfe49c9c38bbb770565
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-13 19:47:05 +0530

    Update q5.js

--- practice_questions/q5.js
+++ practice_questions/q5.js
@@ -1,4 +1,4 @@
-//Write a JavaScript program that displays all even numbers from 0 to 100
+//Write a JavaScript program that displays all even numbers from 0 to 100.
 
 let i=0;
 for(i=0;i<=100;i++)
