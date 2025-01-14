2ca29079c4cf554401aa46eff738a3678fa52619
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-14 21:11:19 +0530

    Update q7.js

--- practice_questions/q7.js
+++ practice_questions/q7.js
@@ -1,3 +1,5 @@
+//Using javascript generate an Unique Username Based on User Input
+
 let fullName=prompt("Enter your full anme without space");
 let userName="@"+fullName+fullName.length;
 console.log(userName);
