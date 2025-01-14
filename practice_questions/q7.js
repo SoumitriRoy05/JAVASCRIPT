d0dcbb123aac9f639039efb037f6345a8829d541
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-14 21:13:50 +0530

    Update q7.js

--- practice_questions/q7.js
+++ practice_questions/q7.js
@@ -1,4 +1,4 @@
-//Using javascript generate an Unique Username Based on User Input
+//Write a program using javascript to generate an Unique Username Based on User Input
 
 let fullName=prompt("Enter your full anme without space");
 let userName="@"+fullName+fullName.length;
