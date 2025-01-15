19638d50ab56bfca54462dee97547ae41402bc5f
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-15 20:10:17 +0530

    Add files via upload

---
+++ practice_questions/q9.js
@@ -0,0 +1,9 @@
+let items=[250,645,300,900,50];
+let idx=0;
+for(let val of items)
+    {
+       let offer=val/10;
+       items[i]=items[i]-offer;
+       console.log(`value after offer=${items[i]}`);
+        i++;
+    }
