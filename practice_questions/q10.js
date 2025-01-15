2f00b73a01e676865765e13092b2227b576a5024
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-15 20:17:20 +0530

    Update q10.js

--- practice_questions/q10.js
+++ practice_questions/q10.js
@@ -1,3 +1,8 @@
+//companies=["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"]
+//1. Using shift() operation remove 1st company from the array
+//2. Using splice() replace third company with "OLA"
+//3. Add "AMAZON" at the end of the array
+
 let companies=["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
 //companies.shift();
 //companies.splice(2,1,"Ola");
