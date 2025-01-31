0184a8d0cb4d5a62ad1cd143c5474d95057c1091
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-31 21:54:56 +0530

    Add files via upload

---
+++ Strings/length.js
@@ -0,0 +1,8 @@
+//word.length
+var name="Soumitri";
+name.length;
+
+// challenge
+var tweet=prompt("Compose your tweet:");
+var tweetCount=tweet.length;
+alert("You have written" + tweetCount + "charecters,you have" + (140- tweetCount)+ "charecters remaining");
