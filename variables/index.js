cfa25f04f36dd4611d580c50a3fcd04adc0efd8a
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-01-31 21:57:45 +0530

    Add files via upload

---
+++ variables/index.js
@@ -0,0 +1,8 @@
+var myName="Soumitri";
+alert(myName);
+var yourNmae="Shinjini";
+console.log("My name is " +myName+ ",Welcome to my course" +yourName + "!");
+var myvar="abc";
+var my123=124;
+var my_name="Soumitri";
+var userScoreFinal=28;
