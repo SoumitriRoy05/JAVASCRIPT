e7f350334e87b915418701ec5b4438fa998604dc
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-02-01 21:51:56 +0530

    Add files via upload

---
+++ Leap_year/leap_year.js
@@ -0,0 +1,25 @@
+function isLeapyear(year)
+{
+    if(year%4===0)
+    {
+        if(year%100===0)
+        {
+            if(year%400===0)
+            {
+                return "Leap year";
+            }
+            else
+            {
+                return "Not a leap year";
+            }
+        }
+        else
+        {
+            return "Leap year";
+        }
+        
+    }
+    else{
+        return "Not a leap year";
+    }
+}
