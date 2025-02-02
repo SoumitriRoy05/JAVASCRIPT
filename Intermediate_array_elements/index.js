84213e6a529385dc50ab5556719798f0fdd4dcdf
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-02-02 21:09:08 +0530

    Add files via upload

---
+++ Intermediate_array_elements/index.js
@@ -0,0 +1,3 @@
+var guestList=["Angela","jack","Pam","James","Lara","Jason"];
+    var guestName=prompt("What is your name?");
+    guestList.includes(guestName);
---
+++ Intermediate_array_elements/index_2.js
@@ -0,0 +1,10 @@
+var guestList=["Angela","jack","Pam","James","Lara","Jason"];
+    var guestName=prompt("What is your name?");
+    if(guestList.includes(guestName))
+    {
+        alert("Welcome!");
+    }
+    else
+    {
+     alert("Sorry, maybe next line");
+    }
