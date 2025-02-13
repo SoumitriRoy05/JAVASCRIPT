15659df2bf277f6395f29fa75f50e9ec382b762b
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-02-13 22:21:44 +0530

    Update index.js

--- BMI_calculator/index.js
+++ BMI_calculator/index.js
@@ -1,3 +1,5 @@
+// Create a JavaScript function that calculates the Body Mass Index (BMI) based on a person's weight (in kilograms) and height (in meters).
+
 function bmiCalculator(weight,height)
 {
     var bmi=weight/(height*height);
