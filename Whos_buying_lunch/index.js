c44e15dbc56eedd42485264693f8b5d927716f20
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-02-14 14:06:24 +0530

    Update index.js

--- Whos_buying_lunch/index.js
+++ Whos_buying_lunch/index.js
@@ -1,3 +1,5 @@
+// You are organizing a lunch outing with a group of friends, and you want to randomly select one person to pay for everyone's meal.Write a function whoPayingLunch(names) that takes an array of names as input and returns a string announcing who will buy lunch today. The selection should be random, ensuring that each person in the list has an equal chance of being chossen.
+
 function whoPayingLunch(names)
 {
     var numberOfPeople=names.length;
